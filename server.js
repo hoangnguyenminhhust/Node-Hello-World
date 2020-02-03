87e91c3d9a62e94c2eed15b71f09f947cbc57a81
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-02-03 15:05:45 +0700

    init

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
-
+const mongoose = require('mongoose')
 const port = process.env.port || 3000
 
+// mongoose.Promise = global.Promise
+// mongoose.connect('mongodb://localhost:27017/db', { useNewUrlParser: true },
+//     (err) => {
+//         if (err) console.log("looi roi")
+//         console.log('Connected')
+//     }
+// )
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+// app.post('/', (req, res) => {
+
+// })
 
 app.listen(port, () => {
     console.log("app run in on", port)
