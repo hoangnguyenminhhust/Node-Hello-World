99e6b2c67133db7d79422adb7ea5086c9616dfb4
Nguyễn Minh Hoàng <48242278+hoangnguyenminhhust@users.noreply.github.com>
2020-02-07 11:04:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const port = process.env.port || 80
+const port = process.env.port || 3000
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
 // mongoose.Promise = global.Promise
